da479469504700d1899d24e78105aefd404a2322
Boris <boris.s@mili.ru>
2017-11-28 22:50:55 +0300

    run web_api and postresql in docker

---
+++ playbook_tengriwallet/roles/www/templates/web_app.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+make_response () {
+echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n
+Hello from my_api!
+$(PGPASSWORD=qwerty psql -U postgres -h 10.20.30.42 -p 5432 -c '\l')"
+}
+while make_response | nc -l "${WEB_API_PORT:-7001}"; do
+echo "================================================"
+done
