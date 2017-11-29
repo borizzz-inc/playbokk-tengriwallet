d269b22921513d03cdbe60086941d4e82f9097f2
Boris <boris.s@mili.ru>
2017-11-29 18:53:01 +0300

    Добавеленно web_app, web_api, nginx reverse proxy

--- playbook_tengriwallet/roles/www/templates/web_app.sh
+++ playbook_tengriwallet/roles/www/templates/web_app.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 make_response () {
 echo -en "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\n\r\n
-Hello from API!
+Hello from MY_APP!
 $(PGPASSWORD=qwerty psql -U postgres -h 10.20.30.42 -p 5432 -c '\l')"
 }
-while make_response | nc -l "${WEB_API_PORT:-7001}"; do
+while make_response | nc -l "${WEB_API_PORT:-8001}"; do
 echo "================================================"
 done
